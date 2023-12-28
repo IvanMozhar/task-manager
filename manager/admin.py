2d723a5292a891182f424bb326c77eecb07f0c7e
Ivan <mozharivan17@gmail.com>
2023-12-28 07:47:16 +0200

    Register models in admin.py

--- manager/admin.py
+++ manager/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from manager.models import Worker, TaskType, Task, Position
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position",)
+
+
+@admin.register(TaskType)
+class TaskTypeAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    search_fields = ("name", )
+
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    search_fields = ("name", )
