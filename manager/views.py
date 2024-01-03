72ddf24304baa47592813eecd99d00d4f8b8b9c5
Ivan <mozharivan17@gmail.com>
2024-01-03 06:21:16 +0200

    Create views for home page and for model Task

--- manager/views.py
+++ manager/views.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from manager.models import Task
+
+
+@login_required
+def index(request):
+    context = {'segment': 'index'}
+    html_template = loader.get_template('home/index.html')
+    return HttpResponse(html_template.render(context, request))
+
+
+class TaskListView(LoginRequiredMixin, generic.ListView):
+    model = Task
+    context_object_name = "task_list"
+    template_name = "manager/task_list.html"
+
+
+class TaskDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Task
+
+
+class TaskCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("manager:task-list")
+
+
+class TaskUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("manager:task-list")
