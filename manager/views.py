bb73d1c8a24fabb308cfd7674faa56c3efc4f0f3
Ivan <mozharivan17@gmail.com>
2024-01-05 04:54:39 +0200

    Add TaskType, Worker views, modified Task view

--- manager/views.py
+++ manager/views.py
@@ -1,12 +1,12 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
-from django.shortcuts import render
 from django.template import loader
 from django.urls import reverse_lazy
 from django.views import generic
 
-from manager.models import Task
+from manager.forms import TaskForm, TaskTypeForm
+from manager.models import Task, Worker, TaskType
 
 
 @login_required
@@ -20,6 +20,7 @@ class TaskListView(LoginRequiredMixin, generic.ListView):
     model = Task
     context_object_name = "task_list"
     template_name = "manager/task_list.html"
+    queryset = Task.objects.select_related("task_type")
 
 
 class TaskDetailView(LoginRequiredMixin, generic.DetailView):
@@ -28,11 +29,40 @@ class TaskDetailView(LoginRequiredMixin, generic.DetailView):
 
 class TaskCreateView(LoginRequiredMixin, generic.CreateView):
     model = Task
-    fields = "__all__"
+    form_class = TaskForm
     success_url = reverse_lazy("manager:task-list")
 
 
 class TaskUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Task
-    fields = "__all__"
+    form_class = TaskForm
     success_url = reverse_lazy("manager:task-list")
+
+
+class TaskTypeListView(LoginRequiredMixin, generic.ListView):
+    model = TaskType
+    context_object_name = "task_type_list"
+    template_name = "manager/task_type_list.html"
+    # check if prefetch related needed
+    queryset = TaskType.objects.prefetch_related("tasks__assignees")
+
+
+class TaskTypeDetailView(LoginRequiredMixin, generic.DetailView):
+    model = TaskType
+
+
+class TaskTypeCreateView(LoginRequiredMixin, generic.CreateView):
+    model = TaskType
+    form_class = TaskTypeForm
+    success_url = reverse_lazy("manager:task-type-list")
+
+
+class TaskTypeDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = TaskType
+    success_url = reverse_lazy("manager:task-type-list")
+
+
+class WorkerListView(LoginRequiredMixin, generic.ListView):
+    model = Worker
+    context_object_name = "worker_list"
+    template_name = "manager/worker_list.html"
