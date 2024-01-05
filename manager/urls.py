0612e74896017ad6d7f5d667fb2ce585a4001a02
Ivan <mozharivan17@gmail.com>
2024-01-05 04:55:15 +0200

    Create urls for new views

--- manager/urls.py
+++ manager/urls.py
@@ -5,7 +5,14 @@ from manager.views import (
     TaskListView,
     TaskCreateView,
     TaskUpdateView,
+    TaskDetailView,
+    WorkerListView,
+    TaskTypeListView,
+    TaskTypeCreateView,
+    TaskTypeDeleteView,
+    TaskTypeDetailView,
 )
+
 
 urlpatterns = [
     path("", index, name="index"),
@@ -21,7 +28,38 @@ urlpatterns = [
     ),
     path(
         "tasks/<int:pk>/update/",
-        TaskUpdateView.as_view()
+        TaskUpdateView.as_view(),
+        name="task-update"
+    ),
+    path(
+        "tasks/<int:pk>/detail/",
+        TaskDetailView.as_view(),
+        name="task-detail"
+    ),
+    path(
+        "workers/",
+        WorkerListView.as_view(),
+        name="worker-list"
+    ),
+    path(
+        "task-types/",
+        TaskTypeListView.as_view(),
+        name="task-type-list"
+    ),
+    path(
+        "task-types/<int:pk>/detail/",
+        TaskTypeDetailView.as_view(),
+        name="task-type-detail"
+    ),
+    path(
+        "task-types/create/",
+        TaskTypeCreateView.as_view(),
+        name="task-type-create"
+    ),
+    path(
+        "task-types/<int:pk>/delete/",
+        TaskTypeDeleteView.as_view(),
+        name="task-type-delete"
     )
 ]
 
