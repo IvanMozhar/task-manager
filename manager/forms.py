578b2d4e9f434cf735c2384648951b9baa335457
Ivan <mozharivan17@gmail.com>
2024-01-07 10:04:49 +0200

    Update form for task creation, create form for worker creation

--- manager/forms.py
+++ manager/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
 
-from manager.models import Task, TaskType
+from manager.models import Task, TaskType, Worker
 
 
 class TaskForm(forms.ModelForm):
@@ -12,10 +13,27 @@ class TaskForm(forms.ModelForm):
 
     class Meta:
         model = Task
-        fields = "__all__"
+        fields = ['name', 'description', 'deadline', 'priority', 'task_type', 'assignees']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'deadline': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'priority': forms.Select(attrs={'class': 'form-select'}),
+            'task_type': forms.Select(attrs={'class': 'form-select'}),
+        }
 
 
 class TaskTypeForm(forms.ModelForm):
     class Meta:
         model = TaskType
         fields = "__all__"
+
+
+class WorkerCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Worker
+        fields = UserCreationForm.Meta.fields + (
+            "position",
+            "first_name",
+            "last_name",
+        )
